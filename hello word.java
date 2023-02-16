569232a8d545d2c57556e9b4aee30a5f07d964bf
GLemoos <guih.lemos.gl@gmail.com>
2023-02-15 21:57:45 -0300

    enviando primeiro teste de controle de versão

---
+++ hello word.java
@@ -0,0 +1,10 @@
+3 public class myfisthelloword  {
+4
+5    public static void mais(string[] args)  {
+6
+7        // todo auto-fenerated method stub
+8         system.out.println("meu primeiro hello word!!!");
+9
+10    }
+11
+12 }        
