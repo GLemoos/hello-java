f72407a8d5b5d14b176bbf08f97d246c23153712
GLemoos <125075894+GLemoos@users.noreply.github.com>
2023-02-16 10:14:45 -0400

    Hello Word

--- hello word.java
+++ hello word.java
@@ -1,10 +1,10 @@
-3 public class myfisthelloword  {
-4
-5    public static void mais(string[] args)  {
-6
-7        // todo auto-fenerated method stub
-8         system.out.println("meu primeiro hello word!!!");
-9
-10    }
-11
-12 }        
+  public class myfisthelloword  {
+ 
+     public static void mais(string[] args)  {
+
+         // todo auto-fenerated method stub
+          system.out.println("meu primeiro hello word!!!");
+
+      }
+
+   }        
